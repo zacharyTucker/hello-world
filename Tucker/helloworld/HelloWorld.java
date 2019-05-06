862c963fd00e62aacfa0e35892dee6eb728710c0
Zach Tucker <zachary.tucker1@pcc.edu>
2019-05-05 20:58:02 -0700

    added main method that prints hello world

--- Tucker/helloworld/HelloWorld.java
+++ Tucker/helloworld/HelloWorld.java
@@ -1,6 +1,16 @@
 package Tucker.helloworld;
 
-
+/**
+ * Author@ Zach Tucker
+ * Version @5/5/2019
+ *
+ * Create class to print hello world
+ */
 
 public class HelloWorld {
+    public static void main (String[] args)
+    {
+
+        System.out.println("Hello World");
+    }
 }
